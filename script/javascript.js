16c58da1fe765c03c3bcde78e9839899c2285be2
Panos <22086296+valavanisp@users.noreply.github.com>
2023-08-08 20:26:14 -0400

    initial logic testing the mouseover events

--- script/javascript.js
+++ script/javascript.js
@@ -1,3 +1,6 @@
+// // hard code for now, later to be used as input
+// let cellsPerRow = 4;
+
 
 for (let i = 0; i < 16; i++) {
     let cell = document.createElement('div');
@@ -5,3 +8,29 @@ for (let i = 0; i < 16; i++) {
     cell.textContent = i;
     document.getElementById('container').appendChild(cell);
 }
+
+// const cell = document.getElementById("container");
+
+// cell.addEventListener("mouseenter", (e) => {
+//     e.target.style.color = "purple";
+// });
+
+
+
+const test = document.getElementById("container");
+
+// This handler will be executed only once when the cursor
+// moves over the unordered list
+test.addEventListener(
+  "mouseover",
+  (event) => {
+    // highlight the mouseenter target
+    event.target.style.background = "purple";
+
+    // reset the color after a short delay
+    setTimeout(() => {
+      event.target.style.background = "";
+    }, 500);
+  },
+  false,
+);
